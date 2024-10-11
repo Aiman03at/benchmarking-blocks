e4ff6e3c29085162b802dabb46b5aaa77e4458e5
Aiman03at <aiman03at@gmail.com>
2024-10-10 21:56:33 -0400

    adding a method for benchmarking

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,5 +1,17 @@
 def benchmark
   # Your benchmarking code goes here.
+  start_time = Time.now
+
+ # Yield to the block that is passed to the method
+ yield
+
+
+end_time = Time.now
+
+# This will return the difference in the timestamps in seconds
+running_time = end_time - start_time
+
+
 end
  
 # Be careful, pasting this into IRB will take a long time to print.
